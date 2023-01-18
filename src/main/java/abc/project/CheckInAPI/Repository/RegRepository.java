333d9324f21e37a7de4c211b3b511631abf9c0fb
klunCheng <klun.CH88@outlook.com>
2023-01-18 18:00:10 +0800

    20230118 K:確認註冊SQL語法

--- src/main/java/abc/project/CheckInAPI/Repository/RegRepository.java
+++ src/main/java/abc/project/CheckInAPI/Repository/RegRepository.java
@@ -20,11 +20,11 @@ public class RegRepository {
 
     public void saveUser(String univ, String depart, String name, String acc,  String pwd, String email, String identity){
         String sql = null;
-        if(identity=="我是學生") {
-            sql = "insert into 學生資料(學校, 科系, 學生姓名, 學號, 密碼, 信箱) values(?,?,?,?,?,?)";
+        if(identity.equals("我是學生")) {
+            sql = "insert into 學生資料([學校], [科系], [學生姓名], [學號], [密碼], [信箱]) values(?,?,?,?,?,?)";
         }
         else {
-            sql = "insert into 老師資料(學校, 科系, 學生姓名, 學號, 密碼, 信箱) values(?,?,?,?,?,?)";
+            sql = "insert into 老師資料([學校],[科系], [學生姓名], [學號], 密碼, 信箱) values(?,?,?,?,?,?)";
         }
         jdbcTemplate.update(sql,univ,depart,name,acc,pwd,email);
         System.out.println("資料已儲存");
