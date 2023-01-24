a44a34a32c794f80c421eab970ba82433d0cdf76
cindy <cindy0909.liu@gmail.com>
2023-01-24 18:27:37 +0800

    小錯誤修正

--- src/main/java/abc/project/CheckInAPI/Controller/CreateClassController.java
+++ src/main/java/abc/project/CheckInAPI/Controller/CreateClassController.java
@@ -3,6 +3,7 @@ package abc.project.CheckInAPI.Controller;
 import abc.project.CheckInAPI.Service.CreateClassService;
 import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,6 +14,7 @@ public class CreateClassController {
     @Autowired
     CreateClassService service;
 
+    @PostMapping("/createclass")
     public JSONObject Creatclass(@RequestBody String body){
         JSONObject object = new JSONObject(body);
         // 將資訊中的 cid 取出
