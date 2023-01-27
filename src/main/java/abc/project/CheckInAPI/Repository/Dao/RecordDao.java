04ce601be8e4ed3ede872e7a88b5083d6bb7f0c8
Cindy <cindy0909.liu@gmail.com>
2023-01-27 16:38:08 +0800

    1/27 修正

--- src/main/java/abc/project/CheckInAPI/Repository/Dao/RecordDao.java
+++ src/main/java/abc/project/CheckInAPI/Repository/Dao/RecordDao.java
@@ -3,7 +3,7 @@ package abc.project.CheckInAPI.Repository.Dao;
 import org.json.JSONObject;
 
 import java.util.List;
-import java.util.Map;1
+import java.util.Map;
 
 public interface RecordDao {
     public List<Map<String, Object>> RollCallRecordForSemester(int cid);
