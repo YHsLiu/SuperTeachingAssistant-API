e8c8d70d099bb7690c05d4278af2146fb80814bb
klunCheng <klun.CH88@outlook.com>
2023-01-27 17:20:45 +0800

    20230127 K:下午存一版

--- src/main/java/abc/project/CheckInAPI/Controller/LotteryController.java
+++ src/main/java/abc/project/CheckInAPI/Controller/LotteryController.java
@@ -1,4 +1,4 @@
-package abc.project.CheckInAPI.Controller;
+/*package abc.project.CheckInAPI.Controller;
 
 import abc.project.CheckInAPI.Service.LotteryService;
 import org.json.JSONObject;
@@ -31,3 +31,4 @@ public class LotteryController {
         lotteryService.resetLottery();
     }
 }
+*/
